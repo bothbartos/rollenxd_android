24ecdfda05caeea1c546057f45e9f6ba215f382f
bartosboth <bothbartos@gmail.com>
2025-04-24 11:21:37 +0200

    created api, repository and models for playlists

---
+++ app/src/main/java/com/bartosboth/rollen_android/data/network/PlaylistAPI.kt
@@ -0,0 +1,18 @@
+package com.bartosboth.rollen_android.data.network
+
+import com.bartosboth.rollen_android.data.model.playlist.Playlist
+import com.bartosboth.rollen_android.data.model.playlist.PlaylistData
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import javax.inject.Singleton
+
+
+@Singleton
+interface PlaylistAPI {
+    @GET("api/playlist/all")
+    suspend fun getPlaylists(): Response<List<PlaylistData>>
+
+    @GET("api/playlist/id/{id}")
+    suspend fun getPlaylistById(@Path("id") id: Long): Response<Playlist>
+}
---
+++ app/src/main/java/com/bartosboth/rollen_android/data/repository/PlaylistRepository.kt
@@ -0,0 +1,42 @@
+package com.bartosboth.rollen_android.data.repository
+
+import android.util.Log
+import com.bartosboth.rollen_android.data.model.playlist.Playlist
+import com.bartosboth.rollen_android.data.model.playlist.PlaylistData
+import com.bartosboth.rollen_android.data.network.PlaylistAPI
+import jakarta.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+class PlaylistRepository @Inject constructor(
+    private val playlistAPI: PlaylistAPI) {
+    suspend fun getPlaylists(): List<PlaylistData> = withContext(Dispatchers.IO) {
+        val response = playlistAPI.getPlaylists()
+
+        if (response.isSuccessful) {
+            response.body() ?: throw IllegalStateException("Response body is null")
+        } else {
+            Log.e(
+                "AudioRepository",
+                "Error getting all songs: ${response.code()} - ${response.message()}"
+            )
+            throw HttpException(response)
+        }
+        response.body()!!
+    }
+
+    suspend fun getPlaylistById(id: Long): Playlist = withContext(Dispatchers.IO) {
+        val response = playlistAPI.getPlaylistById(id)
+
+        if(response.isSuccessful) {
+            response.body() ?: throw IllegalStateException("Response body is null")
+        }else {
+            Log.e(
+                "AudioRepository",
+                "Error getting playlist by id: ${response.code()} - ${response.message()}"
+            )
+            throw HttpException(response)
+        }
+    }
+}
